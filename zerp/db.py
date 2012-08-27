38587e5ebdb04528664d2e5e6be40b48cc02fcee
dimon <dimon@mein-compf.(none)>
2012-08-27 22:31:31 +0400

    Added Issue model.

--- zerp/db.py
+++ zerp/db.py
@@ -1,4 +1,4 @@
-from flask.ext.mongokit import MongoKit, Document
+from flask.ext.mongokit import MongoKit, Document, IS
 
 from zerp import app
 
@@ -13,7 +13,7 @@ class User(Document):
     structure = {
         'name': unicode,
         'password': unicode,
-        'role': unicode,
+        'role': IS(u'admin', u'operator', u'executant'),
         'first_name': unicode,
         'second_name': unicode,
         'phone': unicode
@@ -29,3 +29,15 @@ class Client(Document):
         'address': [unicode],
         'phone': [unicode]
     }
+
+
+@db.register
+class Issue(Document):
+    __collection__ = 'issues'
+
+    structure = {
+        'topic': unicode,
+        'creator': User
+    }
+
+    use_autorefs = True
